2bb136a91be7940b11819af43652c13195d7d3da
DusanCar11 <dusannikolic11@gmail.com>
2019-08-19 14:43:15 +0200

    Initial commit with database ready on azure and prepared for making classes as tables and migrations

---
+++ IEPProjekat/AppContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace IEPProjekat
+{
+    public class AppContext : DbContext
+    {
+        public AppContext() : base("IEP2019")
+        {
+        }
+
+    }
+}
