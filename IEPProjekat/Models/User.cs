1c1233daa4ce688221bc33654b00276028328fb9
Dusan Nikolic <dusannikolic11@gmail.com>
2019-08-20 14:49:29 +0200

    From laptop first commit where you've made all classes and populated user class

---
+++ IEPProjekat/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace IEPProjekat.Models {
+    [Table("Users")]
+    public class User {
+        [Required]
+        [Key]
+        private int Id { get; set; }
+        [Required]
+        private String Name { get; set; }
+        [Required]
+        private String LastName { get; set; }
+        [Required]
+        private String Mail { get; set; }
+        [Required]
+        private String Password { get; set; }
+        [Required]
+        private int Tokens { get; set; }
+        [Required]
+        private String Status { get; set; }
+        [Required]
+        private String Role { get; set; }
+        public virtual ICollection<Question> Questions {get;set;}
+        public virtual ICollection<Reply> Replies { get; set; }
+        public virtual ICollection<Grade> Grades { get; set; }
+        public virtual ICollection<Channel> Channels { get; set; }
+    }
+}
